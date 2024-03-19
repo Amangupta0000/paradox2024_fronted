da6816987f98bd0efe87eb418e06873e612f6535
rishh01 <rishabhsuryavanshi2004@gmail.com>
2024-03-19 20:54:48 +0530

    Coming soon page

--- lib/features/home/screens/home.dart
+++ lib/features/home/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:paradox_2024/features/home/screens/comingsoon.dart';
 import 'package:paradox_2024/features/home/screens/question_card_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,41 +19,52 @@ class HomeScreen extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Center(
-          child: IconButton(onPressed: signOut, icon: Icon(Icons.logout)),
+          child: IconButton(
+            onPressed: signOut,
+            icon: const Icon(Icons.logout),
+          ),
         ),
+        const SizedBox(height: 200),
         GestureDetector(
           onTap: () {
             Navigator.of(context)
-                .push(MaterialPageRoute(builder: (ctx) => Question_Screen()));
+                .push(MaterialPageRoute(builder: (ctx) => const Question_Screen()));
           },
           child: Container(
             height: 60,
             width: 100,
             color: Colors.red,
-            margin: EdgeInsets.all(15),
-            padding: EdgeInsets.all(15),
-            child: Text(
-              'LEVEL 1',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+            margin: const EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
+            child: const Center(
+              child: Text(
+                'LEVEL 1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (ctx) => const comingsoon()));
+          },
           child: Container(
             height: 60,
             width: 100,
             color: Colors.red,
-            margin: EdgeInsets.all(15),
-            padding: EdgeInsets.all(15),
-            child: Text(
-              'LEVEL 2',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+            margin: const EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
+            child: const Center(
+              child: Text(
+                'LEVEL 2',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
