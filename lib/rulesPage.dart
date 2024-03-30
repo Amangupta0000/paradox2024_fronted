55cfac9001c5ff3032b9fb2e82f3f2e51dd493db
rishh01 <rishabhsuryavanshi2004@gmail.com>
2024-03-30 14:23:23 +0530

    rules page added

--- lib/rulesPage.dart
+++ lib/rulesPage.dart
@@ -5,9 +5,106 @@ class rulesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is the Rules Page'),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 2,
+                  child: Text(
+                    'PARADOX',
+                    style: TextStyle(
+                      fontFamily: 'Hermes',
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff802C95),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 2,
+                  left: 0,
+                  child: Text(
+                    'PARADOX',
+                    style: TextStyle(
+                      fontFamily: 'Hermes',
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff802C95),
+                    ),
+                  ),
+                ),
+                Text(
+                  'PARADOX',
+                  style: TextStyle(
+                    fontFamily: 'Hermes',
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFFDE34),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 50,),
+          const Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 2,
+                  child: Text(
+                    'RULES',
+                    style: TextStyle(
+                      fontFamily: 'Hermes',
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff802C95),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 2,
+                  left: 0,
+                  child: Text(
+                    'RULES',
+                    style: TextStyle(
+                      fontFamily: 'Hermes',
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff802C95),
+                    ),
+                  ),
+                ),
+                Text(
+                  'RULES',
+                  style: TextStyle(
+                    fontFamily: 'Hermes',
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFFDE34),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 400,
+              child: Image.asset(
+                'assets/rules.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
