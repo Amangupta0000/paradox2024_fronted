33ba0d099499d663352a28b4b1c2826d2a0a1a4b
rishh01 <rishabhsuryavanshi2004@gmail.com>
2024-04-12 10:16:55 +0530

    Final UI changes and rules added

---
+++ fonts/build/app/generated/source/buildConfig/release/com/example/paradox_2024/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.paradox_2024;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.teamexe.paradox_2024";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
